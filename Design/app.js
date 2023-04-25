2cd955f19b5248275700499a6f04e23859df55bb
kanavjain25 <114650172+kanavjain25@users.noreply.github.com>
2023-04-25 20:45:44 +0530

    Add files via upload

--- Design/app.js
+++ Design/app.js
@@ -1,9 +1,9 @@
 
-        $(function() {
-        $("li").click(function(e) {
-          e.preventDefault();
-          $("li").removeClass("active");
-          $(this).addClass("active");
-        });
+$(function() {
+    $("li").click(function(e) {
+      e.preventDefault();
+      $("li").removeClass("active");
+      $(this).addClass("active");
     });
-      
+});
+  
